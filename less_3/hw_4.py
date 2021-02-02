3fe6574c818323e488a326271d1df651d763a3bb
CoolCigarets LLC <rek771@gmail.com>
2021-02-02 15:02:01 +0300

    hotfix 4 задания

--- less_3/hw_4.py
+++ less_3/hw_4.py
@@ -8,18 +8,18 @@ MAX_ITEM = 1000
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 # print(array)
 
-dict = {}
+dict_ = {}
 max_item = 0
 for item in array:
-    if item in dict:
-        dict[item] += 1
+    if item in dict_:
+        dict_[item] += 1
     else:
-        dict[item] = 1
+        dict_[item] = 1
 
     if max_item == 0:
         max_item = item
-    elif dict[item] > dict[max_item]:
+    elif dict_[item] > dict_[max_item]:
         max_item = item
 
-print(f'Чаще всего {dict[max_item]} раз встречается число {max_item}')
+print(f'Чаще всего {dict_[max_item]} раз встречается число {max_item}')
 # print(dict)
